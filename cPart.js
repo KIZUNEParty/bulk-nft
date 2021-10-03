0a81319b32e092c9302e900103bd606cdbd352f8
ProfDetermined <owner@detzz.in.th>
2021-10-03 13:05:11 +0000

    add part control

---
+++ cPart.js
@@ -0,0 +1,49 @@
+// edit frame width and height
+const width = 2000
+const height = 2000
+
+// img gen amount
+const d = 3
+
+// amount of part 11
+const part11 = Math.floor(Math.random() * 1);
+
+// amount of part 10
+const part10 = Math.floor(Math.random() * 1);
+
+// amount of part 09
+const part09 = Math.floor(Math.random() * 1);
+
+// amount of part 08
+const part08 = Math.floor(Math.random() * 1);
+
+// amount of part 07
+const part07 = Math.floor(Math.random() * 1);
+
+// amount of part 06
+const part06 = Math.floor(Math.random() * 1);
+
+// amount of part 05
+const part05 = Math.floor(Math.random() * 1);
+
+// amount of part 04
+const part04 = Math.floor(Math.random() * 1);
+
+// amount of part 03
+const part03 = Math.floor(Math.random() * 1);
+
+// amount of part 02
+const part02 = Math.floor(Math.random() * 1);
+
+// amount of part 01
+const part01 = Math.floor(Math.random() * 2);
+
+// amount of part 00
+const part00 = Math.floor(Math.random() * 0);
+
+
+
+// No Edit Part!
+const r = Math.floor(Math.random() * 255);
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
