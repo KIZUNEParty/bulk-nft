91cf90190893dc8cfb9daa53c487c62358a611b2
ProfDetermined <owner@detzz.in.th>
2021-10-03 13:13:46 +0000

    re setup content part amount

--- cPart.js
+++ cPart.js
@@ -5,45 +5,58 @@ const height = 2000
 // img gen amount
 const d = 3
 
-// amount of part 11
-const part11 = Math.floor(Math.random() * 1);
-
-// amount of part 10
-const part10 = Math.floor(Math.random() * 1);
-
-// amount of part 09
-const part09 = Math.floor(Math.random() * 1);
-
-// amount of part 08
-const part08 = Math.floor(Math.random() * 1);
-
-// amount of part 07
-const part07 = Math.floor(Math.random() * 1);
-
-// amount of part 06
-const part06 = Math.floor(Math.random() * 1);
-
-// amount of part 05
-const part05 = Math.floor(Math.random() * 1);
-
-// amount of part 04
-const part04 = Math.floor(Math.random() * 1);
-
-// amount of part 03
-const part03 = Math.floor(Math.random() * 1);
-
-// amount of part 02
-const part02 = Math.floor(Math.random() * 1);
+// amount of part 00
+const p00 = 1
 
 // amount of part 01
-const part01 = Math.floor(Math.random() * 2);
+const p01 = 1
 
-// amount of part 00
-const part00 = Math.floor(Math.random() * 0);
+// amount of part 02
+const p02 = 1
+
+// amount of part 03
+const p03 = 1
+
+// amount of part 04
+const p04 = 1
+
+// amount of part 05
+const p05 = 1
+
+// amount of part 06
+const p06 = 1
+
+// amount of part 07
+const p07 = 1
+
+// amount of part 08
+const p08 = 1
+
+// amount of part 09
+const p09 = 1
+
+// amount of part 10
+const p10 = 1
+
+// amount of part 11
+const p11 = 1
 
 
 
 // No Edit Part!
-const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
+const r = Math.floor(Math.random() * 255)
+const g = Math.floor(Math.random() * 255)
+const b = Math.floor(Math.random() * 255)
+
+const part00 = Math.floor(Math.random() * p00)
+const part01 = Math.floor(Math.random() * p01)
+const part02 = Math.floor(Math.random() * p02)
+const part03 = Math.floor(Math.random() * p03)
+const part04 = Math.floor(Math.random() * p04)
+const part05 = Math.floor(Math.random() * p05)
+const part06 = Math.floor(Math.random() * p06)
+const part07 = Math.floor(Math.random() * p07)
+const part08 = Math.floor(Math.random() * p08)
+const part09 = Math.floor(Math.random() * p09)
+const part10 = Math.floor(Math.random() * p10)
+const part11 = Math.floor(Math.random() * p11)
