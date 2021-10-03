8bd86c2337c962b5313ff5978a0ba33e809a7640
ProfDetermined <owner@detzz.in.th>
2021-10-03 00:02:12 +0000

    updated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const { createCanvas }= require('canvas')
 
 const width = 2000
@@ -5,4 +6,8 @@ const height = 2000
 
 const canvas = createCanvas(width, height)
 const context = canvas.getContext('2d')
+context.fillStyle = 'rgb(37, 220, 240)'
+context.fillRect(0, 0, width, height)
 
+const buffer = canvas.toBuffer("image/png");
+fs.writeFileSync("./out/out.png", buffer);
